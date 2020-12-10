eca01e00b741c14b7681fd0a519ca907893773bb
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2020-12-10 18:12:39 -0500

    refactor

--- application.go
+++ application.go
@@ -17,7 +17,7 @@ func main() {
     defer f.Close()
     log.SetOutput(f)
 
-    const indexPage = "/opt/hello/public/index.html"
+    const indexPage = "/opt/goapp/public/index.html"
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 	    if buf, err := ioutil.ReadAll(r.Body); err == nil {
